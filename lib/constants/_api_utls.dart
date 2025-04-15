3788d060d22a661d5616c4552b8b220b0c12dcf7
hubertit <hubert@itdevs.rw>
2025-04-15 20:35:05 +0200

    Improvements

--- lib/constants/_api_utls.dart
+++ lib/constants/_api_utls.dart
@@ -1,3 +1,2 @@
-// const String baseUrl = "https://freshmeals.rw/api/";
-const String mapKy = "AIzaSyCd8042Lj-wdIbCWAsvRp-FctZh3rKKS-s";
 const String baseUrl = "https://test.freshmeals.rw/api/";
+const String mapKy = "AIzaSyCd8042Lj-wdIbCWAsvRp-FctZh3rKKS-s";
